3ba6b5b482e5c183d2f041d8dcfee1c760836912
techno-tanoC <develop.tanoc@gmail.com>
2021-08-15 22:19:59 +0900

    Add Item::all and Item::insert

---
+++ src/item.rs
@@ -0,0 +1,44 @@
+use anyhow::Result;
+use chrono::prelude::*;
+use sqlx::sqlite::SqliteConnection;
+
+#[derive(Debug, Clone)]
+pub struct Item {
+    pub id: i64,
+    pub title: String,
+    pub url: String,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
+
+impl Item {
+    pub async fn all(conn: &mut SqliteConnection) -> Result<Vec<Item>> {
+        let items = sqlx::query_as!(
+            Item,
+            r#"
+            SELECT *
+            FROM items
+            "#
+        )
+        .fetch_all(conn)
+        .await?;
+
+        Ok(items)
+    }
+
+    pub async fn insert(conn: &mut SqliteConnection, title: &str, url: &str) -> Result<i64> {
+        let id = sqlx::query!(
+            r#"
+            INSERT INTO items (title, url)
+            VALUES (?1, ?2)
+            "#,
+            title,
+            url
+        )
+        .execute(conn)
+        .await?
+        .last_insert_rowid();
+
+        Ok(id)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
+mod item;
+
+use anyhow::Result;
 use axum::prelude::*;
+use sqlx::sqlite::SqlitePool;
+use std::env;
 use std::net::SocketAddr;
 
-#[tokio::main]
-async fn main() {
-    let app = route("/", get(root));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
+use crate::item::Item;
 
-async fn root() -> &'static str {
-    "Hello, World!"
+#[tokio::main]
+async fn main() -> Result<()> {
+    let database_url = env::var("DATABASE_URL")?;
+    let pool = SqlitePool::connect(&database_url).await?;
+    let mut conn = pool.acquire().await?;
+
+    Item::insert(&mut conn, "title", "url").await?;
+
+    let items = Item::all(&mut conn).await?;
+    for item in items {
+        dbg!(item);
+    }
+
+    Ok(())
 }
