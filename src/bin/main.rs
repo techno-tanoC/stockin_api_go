fe2517d17fa88037d42e1eacce97f132c1617ccd
techno-tanoC <develop.tanoc@gmail.com>
2021-08-24 19:29:52 +0900

    Respect PORT env var

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,10 +7,12 @@ use stockin_api::*;
 #[tokio::main]
 async fn main() -> Result<()> {
     let database_url = env::var("DATABASE_URL")?;
+    let port = env::var("PORT").unwrap_or("3000".to_string()).parse()?;
+
     let state = new_state(&database_url).await?;
     let app = build_app(state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
