923e42e6fed28367ea2f5917225e63842544032f
techno-tanoC <develop.tanoc@gmail.com>
2023-02-26 00:03:12 +0900

    Add conversions

--- domain/item.go
+++ domain/item.go
@@ -36,3 +36,23 @@ func (params *ItemParams) BuildForUpdate(id UUID) *queries.UpdateItemParams {
 		UpdatedAt: now,
 	}
 }
+
+type Item struct {
+	ID        UUID
+	Title     string
+	URL       string
+	Thumbnail string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func ItemFromModel(model *queries.Item) *Item {
+	return &Item{
+		ID:        model.ID,
+		Title:     model.Title,
+		URL:       model.Url,
+		Thumbnail: model.Thumbnail,
+		CreatedAt: model.CreatedAt,
+		UpdatedAt: model.UpdatedAt,
+	}
+}
--- domain/tag.go
+++ domain/tag.go
@@ -30,3 +30,19 @@ func (params *TagParams) BuildForUpdate(id UUID) *queries.UpdateTagParams {
 		UpdatedAt: now,
 	}
 }
+
+type Tag struct {
+	ID        UUID
+	Name      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+func TagFromModel(model *queries.Tag) *Tag {
+	return &Tag{
+		ID:        model.ID,
+		Name:      model.Name,
+		CreatedAt: model.CreatedAt,
+		UpdatedAt: model.UpdatedAt,
+	}
+}
