614630605163ce593a6db3af3f002c866ffef647
techno-tanoC <develop.tanoc@gmail.com>
2023-03-18 17:20:58 +0900

    Rewrite tests

--- tests/item_test.go
+++ tests/item_test.go
@@ -14,6 +14,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/gofrs/uuid"
 	"github.com/kinbiko/jsonassert"
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
@@ -25,45 +26,141 @@ var (
 	base       = os.Getenv("TESTBASE")
 )
 
-func TestItemFind(t *testing.T) {
+func TestItemCRUD(t *testing.T) {
 	ctx := context.Background()
 	db, release := internal.WithTestDatabase(ctx, base, schemaPath)
 	defer release()
 
-	item, err := internal.CreateItem(ctx, db)
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	app := handlers.BuildApp(db)
-	req := httptest.NewRequest("GET", fmt.Sprintf("/items/%v", item.ID), nil)
-	rec := httptest.NewRecorder()
-	app.ServeHTTP(rec, req)
-
 	asserts := assert.New(t)
 	ja := jsonassert.New(t)
-	asserts.Equal(http.StatusOK, rec.Code)
-	ja.Assertf(
-		rec.Body.String(),
-		`{
-			"data": {
-				"id": "%s",
-				"title": "%s",
-				"url": "%s",
-				"thumbnail": "%s",
-				"created_at": "<<PRESENCE>>",
-				"updated_at": "<<PRESENCE>>"
-			},
-			"message": ""
-		}`,
-		item.ID,
-		item.Title,
-		item.URL,
-		item.Thumbnail,
-	)
+
+	item := internal.NewItem()
+	newItem := internal.NewItem()
+
+	{
+		req := httptest.NewRequest(
+			"POST",
+			"/items",
+			strings.NewReader(fmt.Sprintf(
+				`{
+					"title": "%s",
+					"url": "%s",
+					"thumbnail": "%s"
+				}`,
+				item.Title,
+				item.URL,
+				item.Thumbnail,
+			)),
+		)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		app.ServeHTTP(rec, req)
+
+		asserts.Equal(http.StatusOK, rec.Code)
+		ja.Assertf(
+			rec.Body.String(),
+			`{
+				"data": {
+					"id": "<<PRESENCE>>",
+					"title": "%s",
+					"url": "%s",
+					"thumbnail": "%s",
+					"created_at": "<<PRESENCE>>",
+					"updated_at": "<<PRESENCE>>"
+				},
+				"message": ""
+			}`,
+			item.Title,
+			item.URL,
+			item.Thumbnail,
+		)
+
+		i := &struct {
+			Data struct {
+				ID uuid.UUID `json:"id"`
+			} `json:"data"`
+		}{}
+		json.Unmarshal(rec.Body.Bytes(), i)
+		item.ID = i.Data.ID
+	}
+
+	{
+		req := httptest.NewRequest("GET", fmt.Sprintf("/items/%v", item.ID), nil)
+		rec := httptest.NewRecorder()
+		app.ServeHTTP(rec, req)
+
+		asserts.Equal(http.StatusOK, rec.Code)
+		ja.Assertf(
+			rec.Body.String(),
+			`{
+				"data": {
+					"id": "%s",
+					"title": "%s",
+					"url": "%s",
+					"thumbnail": "%s",
+					"created_at": "<<PRESENCE>>",
+					"updated_at": "<<PRESENCE>>"
+				},
+				"message": ""
+			}`,
+			item.ID,
+			item.Title,
+			item.URL,
+			item.Thumbnail,
+		)
+	}
+
+	{
+		req := httptest.NewRequest(
+			"PUT",
+			fmt.Sprintf("/items/%s", item.ID),
+			strings.NewReader(fmt.Sprintf(`{
+					"title": "%s",
+					"url": "%s",
+					"thumbnail": "%s"
+				}`,
+				newItem.Title,
+				newItem.URL,
+				newItem.Thumbnail,
+			)),
+		)
+		req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+		rec := httptest.NewRecorder()
+		app.ServeHTTP(rec, req)
+
+		asserts.Equal(http.StatusOK, rec.Code)
+		ja.Assertf(
+			rec.Body.String(),
+			`{
+				"data": {
+					"id": "%s",
+					"title": "%s",
+					"url": "%s",
+					"thumbnail": "%s",
+					"created_at": "<<PRESENCE>>",
+					"updated_at": "<<PRESENCE>>"
+				},
+				"message": ""
+			}`,
+			item.ID,
+			newItem.Title,
+			newItem.URL,
+			newItem.Thumbnail,
+		)
+	}
+
+	{
+		req := httptest.NewRequest("DELETE", fmt.Sprintf("/items/%v", item.ID), nil)
+		rec := httptest.NewRecorder()
+		app.ServeHTTP(rec, req)
+
+		asserts := assert.New(t)
+		asserts.Equal(http.StatusNoContent, rec.Code)
+	}
 }
 
-func TestItemsFindByRange(t *testing.T) {
+func TestItemFindByRange(t *testing.T) {
 	ctx := context.Background()
 	db, release := internal.WithTestDatabase(ctx, base, schemaPath)
 	defer release()
@@ -99,132 +196,6 @@ func TestItemsFindByRange(t *testing.T) {
 		items[1].URL,
 		items[1].Thumbnail,
 	)
-}
-
-func TestItemCreate(t *testing.T) {
-	ctx := context.Background()
-	db, release := internal.WithTestDatabase(ctx, base, schemaPath)
-	defer release()
-
-	title := "example"
-	url := "https://example.com/"
-	thumbnail := "https://example.com/image.jpg"
-
-	app := handlers.BuildApp(db)
-	req := httptest.NewRequest(
-		"POST",
-		"/items",
-		strings.NewReader(fmt.Sprintf(
-			`{
-				"title": "%s",
-				"url": "%s",
-				"thumbnail": "%s"
-			}`,
-			title,
-			url,
-			thumbnail,
-		)),
-	)
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	app.ServeHTTP(rec, req)
-
-	asserts := assert.New(t)
-	ja := jsonassert.New(t)
-	asserts.Equal(http.StatusOK, rec.Code)
-	ja.Assertf(
-		rec.Body.String(),
-		`{
-			"data": {
-				"id": "<<PRESENCE>>",
-				"title": "%s",
-				"url": "%s",
-				"thumbnail": "%s",
-				"created_at": "<<PRESENCE>>",
-				"updated_at": "<<PRESENCE>>"
-			},
-			"message": ""
-		}`,
-		title,
-		url,
-		thumbnail,
-	)
-}
-
-func TestUpdateTest(t *testing.T) {
-	ctx := context.Background()
-	db, release := internal.WithTestDatabase(ctx, base, schemaPath)
-	defer release()
-
-	item, err := internal.CreateItem(ctx, db)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	title := "example"
-	url := "https://example.com/"
-	thumbnail := "https://example.com/image.jpg"
-
-	app := handlers.BuildApp(db)
-	req := httptest.NewRequest(
-		"PUT",
-		fmt.Sprintf("/items/%s", item.ID),
-		strings.NewReader(fmt.Sprintf(
-			`{
-				"title": "%s",
-				"url": "%s",
-				"thumbnail": "%s"
-			}`,
-			title,
-			url,
-			thumbnail,
-		)),
-	)
-	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	rec := httptest.NewRecorder()
-	app.ServeHTTP(rec, req)
-
-	asserts := assert.New(t)
-	ja := jsonassert.New(t)
-	asserts.Equal(http.StatusOK, rec.Code)
-	ja.Assertf(
-		rec.Body.String(),
-		`{
-			"data": {
-				"id": "%s",
-				"title": "%s",
-				"url": "%s",
-				"thumbnail": "%s",
-				"created_at": "<<PRESENCE>>",
-				"updated_at": "<<PRESENCE>>"
-			},
-			"message": ""
-		}`,
-		item.ID,
-		title,
-		url,
-		thumbnail,
-	)
-
-}
-
-func TestDeleteTest(t *testing.T) {
-	ctx := context.Background()
-	db, release := internal.WithTestDatabase(ctx, base, schemaPath)
-	defer release()
-
-	item, err := internal.CreateItem(ctx, db)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	app := handlers.BuildApp(db)
-	req := httptest.NewRequest("DELETE", fmt.Sprintf("/items/%v", item.ID), nil)
-	rec := httptest.NewRecorder()
-	app.ServeHTTP(rec, req)
-
-	asserts := assert.New(t)
-	asserts.Equal(http.StatusNoContent, rec.Code)
 }
 
 func TestItemImportAndExport(t *testing.T) {
