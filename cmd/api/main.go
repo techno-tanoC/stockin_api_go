dddd338c44d76cc4b5b36faf96ca72efc8b335a5
techno-tanoC <develop.tanoc@gmail.com>
2023-03-19 14:42:49 +0900

    Add KeyAuth

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"stockin-api/handlers"
 
+	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	_ "github.com/lib/pq"
 )
@@ -17,11 +18,23 @@ func main() {
 		log.Fatal(err)
 	}
 
+	token := os.Getenv("TOKEN")
+	auth := &Auth{token}
+
 	e := handlers.BuildApp(db)
 
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(middleware.KeyAuth(auth.auth))
 
 	e.Logger.Fatal(e.Start(":3000"))
+}
+
+type Auth struct {
+	token string
+}
+
+func (a *Auth) auth(key string, c echo.Context) (bool, error) {
+	return key == a.token, nil
 }
